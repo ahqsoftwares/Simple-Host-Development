1bba0ad7d73f45021f9739305480feac8f2e60d0
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 21:23:11 +0530

    test

--- handlers/interaction/index.js
+++ handlers/interaction/index.js
@@ -2,6 +2,7 @@ function load(module, ...args) {
          require(`./${module}`)(...args);
 }
 module.exports = async function(i, client) {
+         console.log(i);
          if (i.data.custom_id == "verify_me") {
                   load("verify/code")(i);
          } else if (i.data.custom_id == "verify_modal") {
