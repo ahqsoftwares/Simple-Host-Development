ab3f3ad7c8bdb99910c0ee2aa5d4d54f9d7f43d0
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 20:36:55 +0530

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const eris = require("eris");
-const process = require("process");
-process.on("uncaughtException", console.log);
-require("eris-modals")(eris);
+(async() => {
+         const eris = require("eris");
+         const process = require("process");
+         process.on("uncaughtException", console.log);
+         require("eris-modals")(eris);
 
-const client = new eris(process.env.token, {
-         intents: ["all"]
-});
-client.on("ready", () => {
-         console.log("bot is online!");
-});
+         const client = new eris(process.env.token, {
+                  intents: ["all"]
+         });
+         client.connect()
+
+         client.on("ready", () => {
+                  console.log("bot is online!");
+         });
+})()
