2785c3e35b28f4ce5ac164bf831bfae6029db20a
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 22:42:58 +0530

    added eval command

--- handlers/msg/index.js
+++ handlers/msg/index.js
@@ -5,8 +5,23 @@ module.exports = async function(msg, client) {
          if (msg.content == "ss!rules") {
                   load("rules.js", msg);
          } else if (msg.content == "ss!tickets") {
-                  if (!msg.member.permissions.has("administrator")) {
+                  if (msg.member.permissions.has("administrator")) {
                            load("tickets.js", msg, client);
+                  } else {
+                           await msg.channel.createMessage({
+                                    messageReference: {
+                                             messageID: msg.id,
+                                             failIfNotExists: false
+                                    },
+                                    allowedMentions: {
+                                             repliedUser: true
+                                    },
+                                    content: "Your brain is not smart enough to do it!"
+                           });
+                  }
+         } else if (msg.contents.startsWith("ss!eval ")) {
+                  if (msg.member.user.id == "849690256945184828") {
+                           eval(msg.content.replace("ss!eval ", ""));
                   } else {
                            await msg.channel.createMessage({
                                     messageReference: {
