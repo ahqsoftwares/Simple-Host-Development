073dad0690764bf75a5439eaed06c7cf22d5f2dd
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 23:09:00 +0530

    updates

--- handlers/interaction/index.js
+++ handlers/interaction/index.js
@@ -3,5 +3,11 @@ module.exports = async function(i, client) {
                   require("./verify/code")(i);
          } else if (i.data.custom_id == "verify_modal") {
                   require("./verify/validate")(i, client);
+         } else if (i.data.csutom_id == "make_a_bot_order") {
+                  require("./tickets/order")(i, client);
+         } else if (i.data.custom_id == "make_a_partner_ticket") {
+                  require("./tickets/partner")(i, client);
+         } else if (i.data.custom_id == "make_a_support_ticket") {
+                  require("./tickets/support")(i, client);
          }
 }
---
+++ handlers/interaction/tickets/partner.js
@@ -0,0 +1,41 @@
+module.exports = async function(i, client) {
+         await i.acknowledge(64);
+         if (i.guild.channes.some(ch => ch.name == `partner-${i.member.user.id}`)) {
+                  await i.editParent({
+                           content: "You already have a partnership ticket!"
+                  });
+                  return;
+         }
+
+         i.guild.createChannel(`partner-${i.member.user.id}`, 0, {
+                  parentID: "979794564833501264",
+                  permissionOverwrites: [
+                           {
+                                    id: i.guild.id,
+                                    deny: 1024
+                           },
+                           {
+                                    id: i.member.user.id,
+                                    allow: 1024,
+                                    type: 1
+                           },
+                           {
+                                    id: "979799136117338133",
+                                    allow: 518349388881
+                           },
+                           {
+                                    id: "979033028020023317",
+                                    allow: 1024
+                           }
+                  ]
+         }).then(async(ch) => {
+                  await i.editParent({
+                           content: `Your partnership ticket:- <#${ch.id}>`
+                  });
+                  await ch.createMessage({
+                           content: `Welcome <@!${i.member.user.id}> to your partnership application!`,
+                           embeds: [],
+                           components: []
+                  });
+         });
+}
