64bd7c2757b2deba0447205896643bc05a6f3d80
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 21:54:07 +0530

    small code change

--- handlers/interaction/verify/code.js
+++ handlers/interaction/verify/code.js
@@ -1,6 +1,13 @@
 const {set} = require("./database");
 module.exports = async function (i) {
          let result = set(i.member.user.id);
+         if (i.member.roles.includes("979027294888816640")) {
+                  await i.createMessage({
+                           content: "You are already verified!",
+                           flags: 64
+                  });
+                  return;
+         }
          await i.createModal({
                   "title": String(result),
                   "custom_id": "verify_modal",
