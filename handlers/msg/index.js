236b30fb55576a5417c0ef81208fffe71bf26d39
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-27 22:38:05 +0530

    test

--- handlers/msg/index.js
+++ handlers/msg/index.js
@@ -5,8 +5,13 @@ module.exports = async function(msg, client) {
          if (msg.content == "ss!rules") {
                   load("rules.js", msg);
          } else if (msg.content == "ss!tickets") {
-                  if (msg.member.permissions.has("administrator")) {
+                  if (!msg.member.permissions.has("administrator")) {
                            load("tickets.js", msg, client);
+                  } else {
+                           await msg.channel.createMessage({
+                                    messageReference: msg.id,
+                                    content: "Your brain is not smart enough to do it!"
+                           });
                   }
          }
 }
