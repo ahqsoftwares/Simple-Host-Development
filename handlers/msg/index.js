e7331ed6e9a3b43ab95f53f1074a160a79c65b3d
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-28 19:30:03 +0530

    fixes

--- handlers/msg/index.js
+++ handlers/msg/index.js
@@ -1,53 +1,38 @@
 function load(module, ...args) {
-         require(`./${module}`)(...args);
+  require(`./${module}`)(...args);
 }
 module.exports = async function(msg, client) {
-         if (msg.content == "ss!rules") {
-                  load("rules.js", msg);
-         } else if (msg.content == "ss!tickets") {
-                  if (msg.member.permissions.has("administrator")) {
-                           load("tickets.js", msg, client);
-                  } else {
-                           await msg.channel.createMessage({
-                                    messageReference: {
-                                             messageID: msg.id,
-                                             failIfNotExists: false
-                                    },
-                                    allowedMentions: {
-                                             repliedUser: true
-                                    },
-                                    content: "Your brain is not smart enough to do it!"
-                           });
-                  }
-                } else if (msg.content.startsWith("ss!eval ")) {
-                    if (msg.member.user.id == "849690256945184828") {
-                             eval(msg.content.replace("ss!eval ", ""));
-                    } else {
-                             await msg.channel.createMessage({
-                                      messageReference: {
-                                               messageID: msg.id,
-                                               failIfNotExists: false
-                                      },
-                                      allowedMentions: {
-                                               repliedUser: true
-                                      },
-                                      content: "Your brain is not smart enough to do it!"
-                             });
-                   }
-                } else if (msg.content.startsWith("ss!eval2 ")) {
-                    if (msg.member.user.id == "745175427533766726") {
-                             eval(msg.content.replace("ss!eval2 ", ""));
-                    } else {
-                             await msg.channel.createMessage({
-                                      messageReference: {
-                                               messageID: msg.id,
-                                               failIfNotExists: false
-                                      },
-                                      allowedMentions: {
-                                               repliedUser: true
-                                      },
-                                      content: "Your brain is not smart enough to do it!"
-                             });
-                    }
-         }
+  if (msg.content == "ss!rules") {
+           load("rules.js", msg);
+  } else if (msg.content == "ss!tickets") {
+           if (msg.member.permissions.has("administrator")) {
+                    load("tickets.js", msg, client);
+           } else {
+                    await msg.channel.createMessage({
+                             messageReference: {
+                                      messageID: msg.id,
+                                      failIfNotExists: false
+                             },
+                             allowedMentions: {
+                                      repliedUser: true
+                             },
+                             content: "Your brain is not smart enough to do it!"
+                    });
+           }
+         } else if (msg.content.startsWith("ss!eval ")) {
+             if (msg.member.user.id == "849690256945184828" || msg.member.user.id == "745175427533766726") {
+                      eval(msg.content.replace("ss!eval ", ""));
+             } else {
+                      await msg.channel.createMessage({
+                               messageReference: {
+                                        messageID: msg.id,
+                                        failIfNotExists: false
+                               },
+                               allowedMentions: {
+                                        repliedUser: true
+                               },
+                               content: "Your brain is not smart enough to do it!"
+                      });
+            }
+         } 
 }
