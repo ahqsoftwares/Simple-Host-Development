a4e97911ae2f7e5b9ce037f2577633c70a4a394b
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-28 20:48:50 +0530

    fixes

--- handlers/interaction/tickets/order/selected.js
+++ handlers/interaction/tickets/order/selected.js
@@ -6,41 +6,46 @@ module.exports = async function(i) {
                   title: "Please give us some info about your bot!",
                   components: [{
                            type: 1,
-                           components: [{
-                                    type: 4,
+                           components: [{type: 4,
                                     custom_id: "bot_name",
                                     style: 1,
                                     label: "Bot name!",
                                     required: true,
-                                    placeholder: "Clyde!"
-                           }, {
-                                    type: 4,
+                                    placeholder: "Clyde!"}]
+                  }, {
+                           type: 1,
+                           components: [{type: 4,
                                     custom_id: "bot_id",
                                     style: 1,
                                     label: "Bot id!",
                                     required: true,
-                                    placeholder: String(i.member.user.id)
-                           }, {
-                                    type: 4,
+                                    placeholder: String(i.member.user.id)}]
+                  }, {
+                           type: 1,
+                           components: [{type: 4,
                                     custom_id: "bot_color",
                                     style: 1,
                                     label: "Embed color!",
                                     required: false,
-                                    placeholder: "random"
-                           }, {
-                                    type: 4,
+                                    placeholder: "random"}]
+                  }, {
+                           type: 1,
+                           components: [{ type: 4,
                                     custom_id: "bot_status",
                                     label: "Status type and name",
                                     style: 2,
                                     required: false,
-                                    placeholder: "Type: Playing\nName: Simple Host Development"
-                           }, {
-                                    type: 4,
+                                    placeholder: "Type: Playing\nName: Simple Host Development"}]
+                  }, {
+                           type: 1,
+                           components: [{type: 4,
                                     custom_id: "bot_menu",
                                     label: "Help menu url",
                                     style: 1,
-                                    required: false
-                           }, {
+                                    required: false}]
+                  }, {
+                           type: 1,
+                           components: [{
                                     type: 4,
                                     custom_id: "bot_helpmenu",
                                     style: 1,
