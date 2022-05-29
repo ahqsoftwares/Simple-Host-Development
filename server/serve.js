2d562a121c8f2666f2375d1b3b684f9250221ba6
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-29 20:36:10 +0530

    fixes

--- server/serve.js
+++ server/serve.js
@@ -1,7 +1,8 @@
-const app = require("express")().listen(3000);
+const app = require("express")();
 const  {Database} = require("quickmongo");
 const db = new Database(process.env.db);
 
+app.listen(17515);
 app.get("/verify", async(req, res) => {
          if (!(req.headers[`x-license`] == (await(db.get(req.headers[`x-user-id`]))))) {
                   res.send({
