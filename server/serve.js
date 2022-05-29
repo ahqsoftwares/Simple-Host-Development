d0322d5d64f31197af9789b047905ce58dc50fb4
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-29 20:52:45 +0530

    checks

--- server/serve.js
+++ server/serve.js
@@ -1,22 +1,33 @@
 (async() => {
          const app = require("express")();
          const  {Database} = require("quickmongo");
+         let password = "";
+         let chars = [
+                  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+                  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+                  ["@", "!", "%", "&", ")", "(", ":", ";", "**", "(C)", "(R)", ",", "."]
+         ];
+         for (i = 0; i < 150; i++) {
+                  let part = (Math.round(Math.random() * 2));
+                  let num = chars[part][Math.round(Math.random() * part.length)];
+                  password += String(num);
+         }
          const db = new Database(process.env.db);
 
-         db.on("ready", () => console.log("Database ready!"));
+         db.on("ready", () => console.log("Database ready!" + "\nPassword: " + password));
          await db.connect();
 
 
          app.listen(17515);
          app.get("/verify", async(req, res) => {
-                  if (!(req.headers[`x-license`] || req.headers[`x-user-id`])) {
+                  if (!(req.query.code || req.query.user)) {
                            res.send({
                                     type: "Error",
                                     desc: "Invalid args!"
                            });
                            return;
                   }
-                  if (!(req.headers[`x-license`] == (await(db.get(req.headers[`x-user-id`]))))) {
+                  if (!(req.query.code == (await(db.get(req.query.user))))) {
                            res.send({
                                     type: "Error"
                            });
@@ -25,5 +36,27 @@
                   res.send({
                            type: "License Confirmed"
                   });
+         });
+         app.get("/add", async(req, res) => {
+                  if (req.query.author == password) {
+
+                  }
+                  if (!(req.query.code || req.query.user)) {
+                           res.send({
+                                    type: "Error",
+                                    desc: "Invalid args!"
+                           });
+                           return;
+                  }
+                  if (await(db.has(req.query.user))) {
+                           await res.send({
+                                    code: await db.get(req.query.user)
+                           });
+                  } else {
+                           db.set(req.query.user, req.query.code);
+                           await res.send({
+                                    code: req.query.code
+                           });
+                  }
          });
 })()
---
+++ test/index.js
@@ -0,0 +1,5 @@
+for (i = 0; i < 150; i++) {
+         if ((Math.round(Math.random() * 2)) == 0) {
+                  console.log(true);
+         }
+}
