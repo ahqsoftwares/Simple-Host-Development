abbe7642e5efac5644e6821bccc4d6c1e1b08cfd
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-05-29 20:40:19 +0530

    server.js

--- server/serve.js
+++ server/serve.js
@@ -9,10 +9,18 @@
 
          app.listen(17515);
          app.get("/verify", async(req, res) => {
+                  if (!(req.headers[`x-license`] || req.headers[`x-user-id`])) {
+                           res.send({
+                                    type: "Error",
+                                    desc: "Invalid args!"
+                           });
+                           return;
+                  }
                   if (!(req.headers[`x-license`] == (await(db.get(req.headers[`x-user-id`]))))) {
                            res.send({
                                     type: "Error"
                            });
+                           return;
                   }
                   res.send({
                            type: "License Confirmed"
